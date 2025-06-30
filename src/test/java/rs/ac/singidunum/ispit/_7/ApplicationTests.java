9c1bd867e9288a301f1ecbe7691ecd701e4318f4
Alexei Melnikov <alexeidjango@gmail.com>
2025-06-30 15:45:27 +0200

    Initial population

---
+++ src/test/java/rs/ac/singidunum/ispit/_7/ApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.singidunum.ispit._7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
